93cdd649e9b1ea2ddea9f60fed96ae1f2c945634
Thomas Cali <tcali@paul-f3Ar6s8.clusters.42paris.fr>
2025-03-06 14:59:16 +0100

    rm .git from libft repo, added new functions to Push_swap, fixed git issues.

--- Push_swap/Push_swap.h
+++ Push_swap/Push_swap.h
@@ -6,12 +6,17 @@
 /*   By: tcali <tcali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 12:36:51 by tcali             #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/03/04 17:31:37 by tcali            ###   ########.fr       */
+=======
+/*   Updated: 2025/02/20 12:45:57 by tcali            ###   ########.fr       */
+>>>>>>> c07e2e5 (added beggining of file Push_swap.h w/ prototype of linked list.)
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+<<<<<<< HEAD
 # include "libft/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
@@ -23,5 +28,22 @@ typedef struct s_stack
 	struct s_stack	*prev;
 	struct s_stack	*next;
 }					t_stack;
+=======
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_node
+{
+	int	nb;
+	int	index;
+}			t_node;
+
+typedef struct s_stack
+{
+	/* data */
+	t_node	*prev;
+	t_node	*next;
+}			t_stack;
+>>>>>>> c07e2e5 (added beggining of file Push_swap.h w/ prototype of linked list.)
 
 #endif
