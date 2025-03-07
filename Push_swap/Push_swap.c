ad2ae741804c193c710875a8aee9bca46a10d3dc
Thomas Cali <tcali@paul-f3Ar6s5.clusters.42paris.fr>
2025-03-07 16:56:50 +0100

    simplified functions to upgrade efficiency of prgm. Made a fct to fetch the av of the stdin into one single str. A lot easier to manage and understand.

--- Push_swap/Push_swap.c
+++ Push_swap/Push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tcali <tcali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 17:03:52 by tcali             #+#    #+#             */
-/*   Updated: 2025/03/07 16:13:50 by tcali            ###   ########.fr       */
+/*   Updated: 2025/03/07 16:54:41 by tcali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	split_to_nodes(t_list **list, char **stack)
 		i++;
 	}
 }
-
+/*
 //check validity of the str. (Maybe useless...)
 char	*is_str(char *str)
 {
@@ -61,7 +61,7 @@ char	*is_str(char *str)
 		return (NULL);
 	}
 	return (str);
-}
+}*/
 
 //Fct which returns the quantity of nb in the **split (stack) str.
 int	get_stack_size(char **stack)
@@ -77,61 +77,60 @@ int	get_stack_size(char **stack)
 	return (size);
 }
 
+char	*stdin_to_str(int arg_nb, char **args)
+{
+	int		i;
+	char	*tmp;
+	char	*str;
+
+	i = 1;
+	tmp = NULL;
+	str = ft_strdup("");
+	while (i < arg_nb)
+	{
+		tmp = ft_strjoin(str, args[i]);
+		free(str);
+		str = ft_strjoin(tmp, " ");
+		free(tmp);
+		i++;
+	}
+	printf("stdin_to_str : str = [%s]\n", str);
+	return (str);
+}
+
 int	main(int ac, char **av)
 {
 	t_list	*a;
-	t_list	*last_node;
+	char	*input;
 	char	**stack;
 	int		i;
 
 	if (ac < 2)
 	{
-		printf("Must give a list of numbers.");
+		printf("Must give a list of numbers.\n");
 		return (1);
 	}
+	a = NULL;
 	if (ac == 2)
 	{
-		printf("av[1] = %s\n", av[1]);
-		if (is_str(av[1]) != NULL)
-		{
-			stack = ft_split(is_str(av[1]), ' ');
-			if (check_stack_errors(stack, get_stack_size(stack)) == 0)
-			{
-				printf("Error. Stack error.");
-				return (1);
-			}
-			split_to_nodes(&a, stack);
-		}
-		else
-		{
-			printf ("Error. Invalid input.");
-			return (1);
-		}
+		//printf("av[1] = %s\n", av[1]);
+		stack = ft_split(av[1], ' ');
 	}
-	a = NULL;
-	last_node = NULL;
-	i = 1;
 	if (ac > 2)
 	{
-		while (i < ac)
-		{
-			printf("\nav[i] = %s\n", av[i]);
-			stack = ft_split(av[i], ' ');
-			if (check_stack_errors(stack, get_stack_size(stack)) == 0)
-			{
-				printf("Error. Stack error.");
-				return (1);
-			}
-			split_to_nodes(&a, stack);
-			i++;
-		}
-		i = 0;
+		//printf("\nav[%d] = %s\n", i, av[i]);
+		input = stdin_to_str(ac, av);
+		stack = ft_split(input, ' ');
 	}
-	while (stack[i])
+	if (!stack || check_stack_errors(stack, get_stack_size(stack)) == 0)
 	{
-		free(stack[i]);
-		i++;
+		printf("Error. Stack error.\n");
+		return (1);
 	}
+	i = 0;
+	split_to_nodes(&a, stack);
+	while (stack[i])
+		free(stack[i++]);
 	free(stack);
 	return (0);
 }
